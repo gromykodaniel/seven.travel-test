a4687848629192240fdbb8f81b8f4647cde76fd5
Danil <gromykodaniel@yandex.ru>
2024-12-01 19:48:31 +0300

    to prod 2

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.tasks.router import router
 from src.user.router import router as user_router
@@ -7,3 +8,21 @@ app = FastAPI()
 
 app.include_router(router)
 app.include_router(user_router)
+
+
+
+origins = [
+
+    "*",
+
+
+]
+
+app.add_middleware(
+
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
